e3897989431e93dceae3b27150c80210195d2428
VivumEns <VivumEns@outlook.com>
2023-03-02 01:32:17 +0300

    last01 commit

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
 def wsgi_application(environ, start_responce):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
+    start_responce('200 OK', [('Content-Type', 'text/plain')])
     return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding='utf8')]
